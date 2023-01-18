ad30d0bb080d50a56d404c778b721dd704b0594c
vkonstdev <vkonstdev@gmail.com>
2023-01-18 14:56:16 +0700

    Stage 2/5 complete

--- src/com/vkonstdev/hyperskill/asciimirror/Main.java
+++ src/com/vkonstdev/hyperskill/asciimirror/Main.java
@@ -1,8 +1,22 @@
 package com.vkonstdev.hyperskill.asciimirror;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Input the file path:");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input);
+        printCow();
+    }
+
+    public static void printCow() {
+        System.out.println("            ^__^");
+        System.out.println("    _______/(oo)");
+        System.out.println("/\\/(       /(__)");
+        System.out.println("   | w----||    ");
+        System.out.println("   ||     ||    ");
     }
 }
