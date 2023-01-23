a1c0df5597c7dc3d31c40b3ca75b9b13cde1497e
vkonstdev <vkonstdev@gmail.com>
2023-01-23 12:46:35 +0700

    Stage 4/5 complete

--- src/com/vkonstdev/hyperskill/asciimirror/Main.java
+++ src/com/vkonstdev/hyperskill/asciimirror/Main.java
@@ -7,16 +7,31 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
         System.out.println("Input the file path:");
         Scanner sc = new Scanner(System.in);
         String filePath = sc.nextLine();
         try {
             sc = new Scanner(new File(filePath));
             while (sc.hasNext()) {
-                System.out.println(sc.nextLine());
+                //System.out.println(sc.nextLine());
+                list.add(sc.nextLine());
             }
         } catch (FileNotFoundException ex) {
             System.out.println("File not found!");
+        }
+        formTwoPictures(list);
+    }
+
+    private static void formTwoPictures(List<String> list) {
+        String maxString = list.stream().max(Comparator.comparing(String::length)).orElse(null);
+        for (String el : list) {
+            if (el.length() < maxString.length()) {
+                el += " ".repeat(maxString.length() - el.length());
+            }
+            el = el + " | " + el;
+            System.out.println(el);
         }
     }
 
